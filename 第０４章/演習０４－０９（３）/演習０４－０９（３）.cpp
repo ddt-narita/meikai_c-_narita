208a14aae839c4e3470cfb93ce9d9cebe77a28d0
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-04 18:45:47 +0900

    新規コミット　演習０４－０９　（進捗率１００％）作成者　成田修之

---
+++ 第０４章/演習０４－０９（３）/演習０４－０９（３）.cpp
@@ -0,0 +1,27 @@
+//演習０４－０９ キャスト記法
+/*読み込んだ3つの整数の合計と平均を表示する。キャスト記法、関数的記法、
+static_cast演算子を使ったものをそれぞれ作る*/
+//
+//5月4日
+//成田修之
+#include<iostream>
+using namespace std;
+int main()
+{
+	int nInputA,nInputB,nInputC =0; //入力する整数A,B,C
+	cout << "3つの整数の合計と平均を求めます\n";
+	cout << "整数A：";
+	cin >> nInputA;
+	cout << "整数B：";
+	cin >> nInputB;
+	cout << "整数C：";
+	cin >> nInputC;
+
+	//A～Cまでの合計
+	int nSum = nInputA + nInputB +nInputC;
+	//A～Cの平均 (static_cast演算子を使ってnSumを実数に変換)
+	double dAve = static_cast<double>(nSum) / 3;
+
+	cout << "合計は：" << nSum << '\n';
+	cout << "平均は：" << dAve << '\n';
+}
