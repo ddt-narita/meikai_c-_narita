063ba3181c0ece90f2ca459d68ff5c9330108ef6
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-05 13:33:47 +0900

    新規コミット　演習０５－０１　（進捗率１００％）作成者成田修之

---
+++ 第０５章～最後まで/演習０５－０１/演習０５－０１.cpp
@@ -0,0 +1,23 @@
+//演習０５－０１
+//int型の要素数5の配列の要素に先頭から順に5，4，3，2，1、を代入して表示
+//
+//5月5日
+//
+#include<iostream>
+using namespace std;
+int main()
+{
+	int nArrayNumber = 5;		//配列nArrayの要素の数
+	int nArray[nArrayNumber];	//nArrayNumber個の要素を持つ配列nArray
+
+	//各要素にnArrayNumberからカウントダウンして代入
+	for(int i = 0; i < nArrayNumber; i++) {
+		nArray[i] = nArrayNumber - i;
+	}
+
+	//各配列の要素を表示
+	for(int i = 0; i< nArrayNumber; i++) {
+		cout <<"nArray[" << i << "] = "<< nArray[i] << '\n';
+	}
+
+}
