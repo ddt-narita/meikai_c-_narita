8f2c7a4c5706eaf71516a63e23290e7a95f47af4
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-09 18:19:59 +0900

    修正コミット　演習０１－０２　（進捗率１００％）作成者　成田修之

--- e_01_02/src/e_01_02.cpp
+++ e_01_02/src/e_01_02.cpp
@@ -1,11 +1,20 @@
-//演習１－２
-//using指令を削除して作成
-//
-//５月１日
-//成田修之
-#include <iostream>
+/* 演習 01-02
+ * using指令を削除して作成
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月8日
+ * 更新者：成田修之 */
 
-int main(){
+#include <iostream>
+//using namespace std;を除いて処理
+
+int main()
+{
+	//サンプル分をusing指令がないためstd::coutを用いて表示
 	std::cout << "初めてのC++プログラム。\n";
+	//サンプル分をusing指令がないためstd::coutを用いて表示
 	std::cout << "画面に出力しています。\n";
+
+	//返却値0
+	return 0;
 }
