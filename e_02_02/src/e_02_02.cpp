1270904e8aaa485852f079ed544b6b60c249dfb1
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-10 14:45:21 +0900

    修正コミット　演習02--02　（進捗率１００％）　作成者　成田修之

--- e_02_02/src/e_02_02.cpp
+++ e_02_02/src/e_02_02.cpp
@@ -1,19 +1,41 @@
-#include<iostream>						//main
-using namespace std;					/*list2-5のelseを
-										　else ifに変えたらどうなるか検討*/
-										//
-int main(){							//５月１日
-	int n;								//成田修之
-	cout<<"整数値：";
-	cin>>n;
+/* 演習 02-02
+ * list02-05のelseをelse ifにしたらどうなるか検討
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新日：成田修之 */
+#include<iostream>
+using namespace std;
 
-	if(n>0)
-		cout <<"その値は正です。\n";
-	else if(n<0)
-		cout <<"その値は負です。\n";
+int main()
+{
+	//キーボードから読み込むための変数を宣言
+	int nInteger;
+	//入力を促す表示
+	cout	<< "整数値：";
+	//キーボードから入力する
+	cin 	>> nInteger;
 
-	//list2-5ではelseのみで文へ
-	else if(n==0)
-		cout <<"その値は０です。\n";
+	//読み込んだ値が正の時
+	if(nInteger > 0) {
+
+		//正であることを表示
+		cout << "その値は正です。\n";
+
+	//読み込んだ値が負の時
+	} else if(nInteger < 0) {
+
+		//負であることを表示
+		cout << "その値は負です。\n";
+
+	//list2-5ではelseのみで文へ、ここではif(nInteger==0)に書き換える
+	} else if(nInteger == 0) {
+
+		//0であることを表示
+		cout << "その値は0です。\n";
+	}
+
+	//main関数の返却値0
+	return 0;
 }
 //結果：else をelse ifにしても条件が適当であれば結果は変わらない
