970f252bfaaf940880be3f7ca5a1def23259e5ea
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-10 14:43:59 +0900

    修正コミット　演習０２－０１　（進捗率１００％）作成者　成田修之

--- e_02_01/src/e_02_01.cpp
+++ e_02_01/src/e_02_01.cpp
@@ -1,14 +1,31 @@
-#include<iostream>					//main
-using namespace std;				//入力された値の絶対値を求める
-									//
-int main(){						//5月１日
-	int x;							//成田修之
-	cout<<"整数値：";
-	cin>>x;
+/* 演習 02-01
+ * 入力された値の絶対値を求める
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之
+ */
+#include<iostream>
+using namespace std;
 
-	//xの値が正ならそのまま負なら符号を変えてその値を表示
-	if(x>0)
-		cout<<"その絶対値は"<<x<<"です。\n";
-	else
-		cout<<"その絶対値は"<<-x<<"です。\n";
+int main()
+{
+	//入力するのに使う変数を宣言
+	int nInput;
+	//整数の入力を促す表示
+	cout 	<< "整数値：";
+	//キーボードから入力
+	cin		>> nInput;
+
+	//nInputの値が負の時
+	if(nInput < 0) {
+		//その値に-を付けてを表示する
+		cout << "その絶対値は" << -nInput << "です。\n";
+	}
+
+	//負でない場合はその値をそのまま表示
+	cout << "その絶対値は" << nInput << "です。\n";
+
+	//main関数の返却値0
+	return 0;
 }
