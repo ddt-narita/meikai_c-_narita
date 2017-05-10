a59a0caa0003d72d16517804ff599cd9888b1076
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-10 13:03:37 +0900

    修正コミット　演習０１－１１　（進捗率１００％）作成者　成田修之

--- e_01_11/src/e_01_11.cpp
+++ e_01_11/src/e_01_11.cpp
@@ -1,16 +1,32 @@
-#include<iostream>									//main
-using namespace std;								/*キーボードから読み込んだ
-													実数値の合計と平均*/
-													//
-int main(){										//5月1日
-	//ｘとｙを実数値を扱えるdouble型で宣言			//成田修之
-	double x,y;
-	cout<<"ｘの値：";
-	cin>>x;
-	cout<<"ｙの値：";
-	cin>>y;
+/*演習 01-11
+ * キーボードから読み込んだ実数値の合計と平均
+ *作成日：5月1日
+ *作成者：成田修之
+ *更新日：5月8日
+ *更新日：成田修之 */
+#include<iostream>
+using namespace std;
 
-	//double型なので平均でも実数が扱えることを確認
-	cout<<"合計は"<<x+y<<"です。\n";
-	cout<<"平均は"<<(x+y)/2<<"です。\n";
+int main()
+{
+	double nInput_1;	//実数で入力する変数1
+	double nInput_2;	//実数で入力する変数2
+
+	//nInput_1に入力を促す
+	cout	<< "nInput_1の値：";
+	//キーボードから入力
+	cin 	>> nInput_1;
+
+	//nInput_2に入力を促す表示と入力
+	cout 	<< "nInput_2の値：";
+	//キーボードから入力
+	cin 	>> nInput_2;
+
+	//キーボードから入力された値の合計を表示
+	cout 	<< "合計は" << nInput_1 + nInput_2 			<< "です。\n";
+	//double型なので平均に実数が表示されることの確認
+	cout 	<< "平均は" << (nInput_1 + nInput_2) / 2 	<< "です。\n";
+
+	//返却値０
+	return 0;
 }
