1be9b8a3f0931fcc907072b7f248b9101990b8de
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-10 13:05:07 +0900

    修正コミット　演習０１－１３　（進捗率１００％）作成者　成田修之

--- e_01_13/src/e_01_13.cpp
+++ e_01_13/src/e_01_13.cpp
@@ -1,24 +1,43 @@
-//ランダムな値を生成するために上二つのヘッダーをインクルードする
-#include<ctime>						//main
-#include<cstdlib>						//いろいろなランダムな整数を生成
-#include<iostream>						//
-										//5月1日
-using namespace std;					//成田修之
+/* 演習 01-13
+ * ランダムな値を生成するために上二つのヘッダーをインクルードする
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 作成者：成田修之
+ */
 
-int main(){
+//time-t型で現在の時間を種にするためのものが入っているヘッダ
+#include<ctime>
+//srand関数の入ったc標準ライブラリのヘッダ
+#include<cstdlib>
+#include<iostream>
+using namespace std;
 
+int main()
+{
 	//ランダムな値を生成するrandの種(基準)を設定
 	srand(time(NULL));
 
+	int nRucky1;	//1桁の正のランダムな整数
+	int nRucky2;	//1桁の負のランダムな整数
+	int nRucky3;	//2桁の正のランダムな整数
+
+
 	//1桁の正の整数をランダムに生成
-	int rucky1 = rand()%9+1;
-	cout<<rucky1<<"\n";
+	nRucky1 = rand() % 9 + 1;
+	//生成した1桁の正の整数を表示
+	cout << nRucky1 << "\n";
 
 	//1桁の負の整数をランダムに生成
-	int rucky2 = rand()%9-9;
-	cout<<rucky2<<"\n";
+	nRucky2 = rand() % 9 - 9;
+	//生成した1桁の負の整数を表示
+	cout << nRucky2 << "\n";
 
 	//2桁の正の整数をランダムに生成
-	int rucky3 = rand()%90+10;
-	cout<<rucky3<<"\n";
+	nRucky3 = rand() % 90 + 10;
+	//生成した2桁の正の整数を表示
+	cout << nRucky3 << "\n";
+
+	//返却値0
+	return 0;
 }
