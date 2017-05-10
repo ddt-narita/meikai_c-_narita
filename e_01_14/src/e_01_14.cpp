e9f9fb5b134b41ce221dbea0942e230302cceaa3
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-10 13:06:01 +0900

    修正コミット　演習０１－１４　（進捗率１００％）作成者　成田　修之

--- e_01_14/src/e_01_14.cpp
+++ e_01_14/src/e_01_14.cpp
@@ -1,24 +1,38 @@
-#include<ctime>					//main関数
-#include<cstdlib>					//入力した値の±５のランダムな値を生成する
-#include<iostream>					//
-									//5月1日
-using namespace std;				//成田修之
+/* 演習 01-14
+ * 入力した値の±5のランダムな値を生成する
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之*/
 
-int main(){
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
 
-	srand(time(NULL));
+using namespace std;
 
-	//±５の数を作るにはその数-5～5の０を含めた11個のうちのランダムな数を生成
-	//またrandは正の０～の値しか作らないのでその値から-5すれば-5～5のランダムな
+int main()
+{
+	//ランダムな値を生成する種を決める
+	srand( time(NULL) );
+
+	//±5の数を作るにはその数-5～5の0を含めた11個のうちのランダムな数を生成
+	//またrandは正の0～の値しか作らないのでその値から-5すれば-5～5のランダムな
 	//値が生成できる
-	int rucky= rand()%11-5;
+	int rucky = rand() % 11 - 5;
 
-	int x;
+	//キーボードから入力するための変数
+	int nInput;
 
-	cout<<"整数値：";
-	cin >> x;
+	//入力を促すための表示
+	cout	<< "整数値：";
+	//キーボードから入力
+	cin 	>> nInput;
 
-	//xに上で生成した-5～5の数を加えればいい
-	cout<<"その値の±５の乱数を生成しました。それは"<<x+rucky<<"です。\n";
+	//nInputに上で生成したrucky:-5～5の数を加えればいい
+	cout	<< "その値の±５の乱数を生成しました。それは";
+	cout	<< nInput + rucky << "です。\n";
 
+	//返却値0
+	return 0;
 }
