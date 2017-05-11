3030a12f93b43cbd079d9063fcd143bcbcb4a4d7
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:21:24 +0900

    修正コミット　演習02-07　（進捗率100％）作成者　成田修之

--- e_02_07/src/e_02_07.cpp
+++ e_02_07/src/e_02_07.cpp
@@ -1,15 +1,31 @@
-#include <iostream>					//main
-using namespace std;					//2つの実数値のうち大きいほうを表示
-										//
-int main() {							//5月1日
-										//成田修之
-	//実数値なのでdouble型で宣言する
-	double a,b;
+/* 演習 02-07
+ * 2つの実数のうち大きいほうを表示
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之 */
 
-	cout <<"実数値a：";		cin >>a;
-	cout <<"実数値b："; 	cin >>b;
+#include <iostream>
+using namespace std;
 
-	//条件演算子を使ってmaxを評価する
-	double max = a>b?a:b;
-	cout <<"大きいほうの値は"<<max<<"です。\n";
+int main()
+{
+	double dblInputA;	//入力された実数値の大小を求めるのに使う変数A
+	double dblInputB;	//入力された実数値の大小を求めるのに使う変数B
+
+	//実数値aにキーボードからの入力を促す文
+	cout 	<< "実数値a：";
+	//キーボードから入力
+	cin 	>> dblInputA;
+	//実数値bにキーボードからの入力を促す表示
+	cout 	<< "実数値b：";
+	//キーボードから入力
+	cin 	>> dblInputB;
+
+	//条件演算子を使って大きいほうの値を表示する
+	cout << "大きいほうの値は" << (dblInputA > dblInputB ? dblInputA : dblInputB);
+	cout << "です。\n";
+
+	//main関数の返却値0
+	return 0;
 }
