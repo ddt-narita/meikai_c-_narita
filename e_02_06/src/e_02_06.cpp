b04264bbfd8380cfdeac8b711117372ffb845101
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:20:47 +0900

    修正コミット　演習02-06　（進捗率100％）　作成者　成田修之

--- e_02_06/src/e_02_06.cpp
+++ e_02_06/src/e_02_06.cpp
@@ -1,22 +1,43 @@
-#include<iostream>						//main
-using namespace std;					//点数に応じて判定を出す
-										//
-int main (){							//5月1日
-										//成田修之
+/* 演習 02-06
+ * 点数に応じて判定を出す
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之 */
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
 	//キーボードから点数を入力
-	int t;
-	cout <<"点数を判定します。\n何点ですか：";
-	cin>>t;
+	int nInput;
+	//何点であるか入力をうながす表示
+	cout 	<< "点数を判定します。\n何点ですか：";
+	//キーボードから入力
+	cin 	>> nInput;
 
+	//点数が0～59点の時
+	if( nInput >= 0 && nInput <= 59) {
+		//判定が不可であることを表示
+		cout << "不可\n";
 
-	//点数に応じて判定 &&は"かつ"の意味
-	if( t>=0 && t<=59)
-		cout <<"不可\n";
-	else if ( t>=60 && t<=69 )
-		cout <<"可\n";
-	else if ( t>=70 && t<=79 )
-		cout <<"良\n";
-	else if ( t>=80 && t<=100 )
-		cout <<"優\n";
+	//点数が60～69の時
+	} else if ( nInput >= 60 && nInput <= 69 ) {
+		//判定が可であることを表示
+		cout << "可\n";
 
+	//点数が70～79の時
+	} else if ( nInput >= 70 && nInput <= 79 ) {
+		//判定が良であることを表示
+		cout << "良\n";
+
+	//80～100の時
+	} else if ( nInput >= 80 && nInput <= 100 ) {
+		//判定が優であることを表示
+		cout << "優\n";
+	}
+
+	//main関数の返却値0
+	return 0;
 }
