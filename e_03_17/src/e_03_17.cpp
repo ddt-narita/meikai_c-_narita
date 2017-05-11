62c4d067bd72e40b189d19aad99dc4e3aa1e7e8e
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 16:17:58 +0900

    新規コミット　演習03-17　（進捗率100%）作成者　成田修之

--- e_03_17/src/e_03_17.cpp
+++ e_03_17/src/e_03_17.cpp
@@ -1,34 +1,63 @@
-#include<string>					//main
-#include<iostream>					/*list3-1を月の読み込みにおいて1～12以外が
-									入力されたとき再入力を求めるようにする*/
-using namespace std;				//
-									//5月3日
-int main ()						//成田修之
-{
-	string strRetry;
+/* 演習03-17
+ * list3-1を月を読み込む際1から12以外が入力されたときに再入力を求めるようにする
+ * 作成日：5月3日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
 
-	//入力された値の季節を求めるのをretryにYかｙが入力される限り繰り返す
+//文字列を扱うstring型を含んだヘッダ
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	string strRetry;	//繰り返すかどうか入力する際に使う変数
+
+	//入力された値の季節を求めるのstrRetryにYかｙが入力される限り繰り返す
 	do{
 		int nMonth;
 
 		//1～12以外の数が入力されると繰り返す
 		do{
-			cout <<"季節を求めます。\n何月ですか：";
-			cin >> nMonth;
-		}while(nMonth<1||nMonth>12);
+			//判定する月の入力を促す表示
+			cout	<< "季節を求めます。\n何月ですか：";
+			//キーボードから入力
+			cin		>> nMonth;
 
-		if(nMonth>=3&&nMonth<=5)
-			cout<<"それは春です。\n";
-		else if (nMonth>=6&&nMonth<=8)
-			cout<<"それは夏です。\n";
-		else if(nMonth>=9&&nMonth<=11)
-			cout<<"それは秋です。\n";
-		else
-			cout<<"それは冬です。\n";
+		//入力された値が1から12以外の数値の時繰り返す
+		} while(nMonth < 1 || nMonth > 12);
 
-		cout<<"もう一度？Y…Yes/N…No：";
-		cin>> strRetry;
-	}//Yかyが入力されれば繰り返す
-	while(strRetry=="Y"||strRetry=="y");
+		//入力された値が3から5の時
+		if(nMonth >= 3 && nMonth <= 5){
+			//春であることを表示
+			cout << "それは春です。\n";
 
+		//入力された値が6から8の時
+		} else if (nMonth >= 6 && nMonth <= 8){
+			//夏であることを表示
+			cout << "それは夏です。\n";
+
+		//入力された値が9から11の時
+		} else if(nMonth >= 9 && nMonth <= 11){
+			//秋であることを表示
+			cout << "それは秋です。\n";
+
+		//それ以外の時（12と1と2）
+		} else {
+			//冬であることを表示
+			cout << "それは冬です。\n";
+		}
+
+		//判定をもう一度繰り返すかを入力を促す表示
+		cout	<<	"もう一度？Y…Yes/N…No：";
+		//繰り返すならYかyを終了するならNかnをキーボードから入力
+		cin		>> strRetry;
+
+	//Yかyが入力されれば繰り返す
+	} while(strRetry == "Y" || strRetry == "y");
+
+	//main関数の返却値0
+	return 0;
 }
