5f29677b830b7ffb00ccb0c792aa036777cfd953
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 16:17:14 +0900

    修正コミット　演習03-16　（進捗率100%）作成者　成田修之

--- e_03_16/src/e_03_16.cpp
+++ e_03_16/src/e_03_16.cpp
@@ -1,17 +1,34 @@
-#include<iostream>				//main
-using namespace std;			//1～nまでの整数値の2乗を表示する
-								//
-int main(){					//5月3日
-								//成田修之
-	int n;
+/* 演習 03-16
+ * 1から入力した値までの整数値を2乗して表示する
+ * 作成日：5月3日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int nInputN;	//入力した値を読み込むのに使う変数
+	int i;			//カウントするのに使う変数
 
 	//1からnまでなのでnが1以上になるまで繰り返す
 	do{
-		cout<<"nの値：";
-		cin>>n;
-	}while(n<0);
+		//入力を促す表示
+		cout 	<< "nの値：";
+		//キーボードから入力
+		cin 	>> nInputN;
+	//入力された値が1以上でないければ繰り返す
+	} while(nInputN < 1);
 
-	//iを１から始めてiとiの2乗を表示する
-	for(int i=1 ; i<=n ; i++)
-		cout<<i<<"の2乗は"<<i*i<<'\n';
+	//1から入力された値まで繰り返す
+	for(i = 1; i <= nInputN; i++) {
+		//iとiの2乗を表示する
+		cout << i << "の2乗は" << i * i << '\n';
+	}
+
+	//main関数の返却値0
+	return 0;
 }
