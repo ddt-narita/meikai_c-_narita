75ed3dd9d68c394f9a1430ebb5c84fb28e6ac9bb
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:25:23 +0900

    修正コミット　演習02-12　（進捗率100%）作成者　成田修之

--- e_02_12/src/e_02_12.cpp
+++ e_02_12/src/e_02_12.cpp
@@ -1,31 +1,48 @@
-#include<iostream>						//main
-using namespace std;					/*2つの整数を読み込み大きいほう小さいほう
-										を表示。ただし同じ値なら同じ値だと表示。*/
- int main(){							//
-	 	 	 	 	 	 	 	 		//5月1日
-	 int a,b;							//成田修之
-	  cout<<"整数a：";	cin>>a;
-	  cout<<"整数b：";	cin>>b;
+/* 演習 02-12
+ * 2つの整数を読み込み大きいほう小さいほうを表示。ただし同じ値なら同じ値だと表示
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之
+ */
 
-	  //maxとminを宣言しておく
-	  int min,max;
+#include<iostream>
+using namespace std;
 
-	  //aとｂどちらがでかいか評価し、小さいほうをminでかいほうをmaxに代入
-	  if(a>b){
-			  min=b;
-			  max=a;
-		  }
-	  else{
-			  min=a;
-			  max=b;
-		  }
+ int main()
+ {
+	 int nInputA;	//大小を比べるのに使う整数A
+	 int nInputB;	//大小を比べるのに使う整数B
 
-	  //もしaとbの値が同じなら同じだと表示し、そうでなければ
-	  //小さいほうと大きいほうを表示する。
-	  if(a==b)
-		  cout<<"2つの値は同じです。\n";
-	  else{
-		  cout<<"小さいほうの値は"<<min<<"です。\n";
-		  cout<<"大きいほうの値は"<<max<<"です。\n";
-	  }
+	 //整数Aに入力を促す表示
+	 cout	<< "整数a：";
+	 //キーボードから入力
+	 cin 	>> nInputA;
+	 //整数Bに入力を促す表示
+	 cout	<< "整数b：";
+	 //キーボードから入力
+	 cin 	>> nInputB;
+
+	 //aとbの値が同じ時
+	 if(nInputA == nInputB) {
+		 //同じであることを表示
+		 cout << "2つの値は同じです。\n";
+
+	 //Aのほうが大きいとき
+	 } else if(nInputA > nInputB) {
+		 //小さいほうBを表示
+		 cout << "小さいほうの値は" << nInputB << "です。\n";
+		 //大きいほうAを表示
+		 cout << "大きいほうの値は" << nInputA << "です。\n";
+
+	 //Bのほうが大きいとき
+	 } else {
+		 //小さいほうAを表示
+		 cout << "小さいほうの値は" << nInputA << "です。\n";
+		 //大きいほうBを表示
+		 cout << "大きいほうの値は" << nInputB << "です。\n";
+	 }
+
+	 //main関数の返却値0
+	 return 0;
  }
