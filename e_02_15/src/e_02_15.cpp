d198fadaa86ebec43d9c2c222582377f7f099cd0
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:27:22 +0900

    修正コミット　演習02-15　（進捗率100%）作成者　成田修之

--- e_02_15/src/e_02_15.cpp
+++ e_02_15/src/e_02_15.cpp
@@ -1,25 +1,45 @@
-#include<ctime>				//main
-#include<cstdlib>				//おみくじプログラム
-#include<iostream>				//
-								//5月2日
-using namespace std;			//成田修之
+/* 演習 02-15
+ * おみくじプログラムの作成
+ * 作成日：5月2日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之
+ *  */
 
-int main(){
-	srand(time(NULL));
+#include<ctime>
+#include<cstdlib>
+#include<iostream>
 
-	//7つのランダムな値を作成
-	int rucky = rand()%7;
+using namespace std;
+
+int main()
+{
+	int nRucky;	//0～6のランダムな値を代入し判定に使う
+
+	//ランダムな値を生成する種を設定
+	srand( time(NULL) );
+
+	//0～6の7つの中からランダムな値を作成
+	nRucky = rand() % 7;
 
 	//生成された値によってラベルに飛ぶ
-	switch(rucky){
-	case 0: cout<<"大吉\n";		break;
-	case 1: cout<<"中吉\n";		break;
-	case 2: cout<<"小吉\n";		break;
-	case 3: cout<<"吉\n";		break;
-	case 4: cout<<"末吉\n";		break;
-	case 5: cout<<"凶\n";		break;
-	case 6: cout<<"大凶\n";		break;
-
+	switch(nRucky) {
+	//0の時 大吉と表示
+	case 0: cout << "大吉\n";		break;
+	//1の時 中吉と表示
+	case 1: cout << "中吉\n";		break;
+	//2の時 小吉と表示
+	case 2: cout << "小吉\n";		break;
+	//3の時 吉と表示
+	case 3: cout << "吉\n";			break;
+	//4の時 末吉と表示
+	case 4: cout << "末吉\n";		break;
+	//5の時 凶と表示
+	case 5: cout << "凶\n";			break;
+	//6の時 大凶と表示
+	case 6: cout << "大凶\n";		break;
 	}
 
+	//main関数の返却値0
+	return 0;
 }
