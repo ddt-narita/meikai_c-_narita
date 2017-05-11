34c5bd932cb525f0896cfa41568d48db12c2d114
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:28:08 +0900

    修正コミット　演習02-16　（進捗率100%）作成者　成田修之

--- e_02_16/src/e_02_16.cpp
+++ e_02_16/src/e_02_16.cpp
@@ -1,28 +1,50 @@
-#include<iostream>					//main
-using namespace std;				//入力された月の季節を判定
-									//
-int main(){						//5月2日
-									//成田修之
-	int n;
+/* 演習 02-16
+ * 入力された月の季節の判定
+ * 作成日：5月2日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之
+ */
 
-	//キーボードから１～１２の数を入力
-	cout<<"１～１２の整数を入力：";
-	cin>>n;
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int nMonth;		//月を入力する変数
+
+	//１～１２の数を入力するように促す表示
+	cout << "１～１２の整数を入力：";
+	//キーボードから入力
+	cin >> nMonth;
 
 	//入力された値の月の季節を表示
 	//１２や１などラベルのあとbreak文がないのでbreak文があるところまで落ちていく
-	switch(n){
-	case 12:
-	case 1:
-	case 2: cout<<"冬";		break;
-	case 3:
-	case 4:
-	case 5: cout<<"春";		break;
-	case 6:
-	case 7:
-	case 8: cout<<"夏";		break;
-	case 9:
-	case 10:
-	case 11:cout<<"秋";		break;
+	switch(nMonth) {
+		//break文がないので下へ
+		case 12:
+		case  1:
+		//入力された値が12～2の時 冬と表示
+		case  2: cout << "冬";		break;
+
+		//break文がないので下へ
+		case  3:
+		case  4:
+		//入力された値が3～5の時 春と表示
+		case  5: cout << "春";		break;
+
+		//break文がないので下へ
+		case  6:
+		case  7:
+		//入力された値が6～8の時 夏と表示
+		case  8: cout << "夏";		break;
+
+		//break文がないので下へ
+		case  9:
+		case 10:
+		//入力された値が9～11の時 秋と表示
+		case 11: cout << "秋";		break;
 	}
+	//main関数の返却値0
+	return 0;
 }
