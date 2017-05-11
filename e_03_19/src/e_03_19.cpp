bbb3f6a6c3471b6d1716e7924d8ea4de41e4d5ee
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 16:19:39 +0900

    修正コミット　演習03-19　（進捗率100%）作成者　成田修之

--- e_03_19/src/e_03_19.cpp
+++ e_03_19/src/e_03_19.cpp
@@ -1,23 +1,41 @@
-#include<iostream>						//main
-using namespace std;					//n段の正方形を作る
-										//
-int main()								//5月3日
-{										//成田修之
-	//n段の正方形を作るのでｎが正になるまで繰り返す
-	int nSquear;
-	for( ; nSquear<1; ){
-		cout<<"n段の正方形を表示します\nnの値：";
-		cin>>nSquear;
+/* 演習03-19
+ * n段の正方形を作る
+ * 作成日：5月3日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	int nSquare;	//何段の正方形を作るか入力するのに使う変数
+
+	//入力された値が1より低い値の限り繰り返す
+	for( ; nSquare < 1; ) {
+		//正方形を表示することを明示
+		cout 	<< "n段の正方形を表示します\n";
+		//何段にするか入力を促す表示
+		cout 	<< "nの値：";
+		//キーボードから入力
+		cin 	>> nSquare;
 	}
 
-	//nSquear1が1～nまでの間以下を繰り返す
-	for(int nSquear1 = 1; nSquear1 <= nSquear; nSquear1++ ){
 
-		//nSquear2が1～nの間＊を表示を繰り返す
-		for(int nSquear2 = 1; nSquear2 <= nSquear; nSquear2++){
+	//iが1～nまでの間以下を繰り返す
+	for(int i = 1; i <= nSquare; i++ ){
+		//jが1～nの間＊を表示を繰り返す
+		for(int j = 1; j <= nSquare; j++){
+			//アスタリスクの表示を繰り返す
 			cout << "*";
 		}
-		//nSquear2が表示を繰り返し終わりnSquear1が一つ直前に改行文字を出力する
+		//jが表示を繰り返し終わりjが一つ増える直前に改行文字を出力する
 		cout<<"\n";
 	}
+
+	//main関数の返却値0
+	return 0;
 }
