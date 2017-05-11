1577cf3c4ef375a0c611007cbbeaad2d4a162804
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 15:44:04 +0900

    新規コミット　演習06-04　（進捗率100%）作成者　成田修之

---
+++ e_06_04/src/e_06_04.cpp
@@ -0,0 +1,40 @@
+/*演習０６－０４
+ * 半径ｒの円の面積を求めて返却する関数circ_areaを作成 （円周率は3.14)
+ * 作成日：５月９日
+ * 作成者：成田修之
+ */
+#include<iostream>
+using namespace std;
+
+/*関数circ_area
+ * 半径rの円の面積を求めて返却する
+ * 引数はint型整数
+ * 返却値はdouble型の面積
+ * 作成日：５月９日
+ * 作成者：成田修之
+ */
+double circ_area(int nRadius)
+{
+	//πは定数なので定数オブジェクトとして宣言し初期化
+	const double dblPI = 3.14;
+	//面積を求める（半径＊半径＊π）
+	double dblCirc_Area = nRadius * nRadius * dblPI;
+
+	//返却値dblCirc_Areaを返却
+	return dblCirc_Area;
+}
+
+int main()
+{
+	//半径を求める
+	int nRadius;
+	//半径の入力を促す表示
+	cout << "半径ｒの面積を求めます\n"
+			"半径：";
+	//キーボードから入力
+	cin >> nRadius;
+
+	//面積を表示
+	cout << "半径" << nRadius << "の面積は：" << circ_area(nRadius) << "です。\n";
+}
+
