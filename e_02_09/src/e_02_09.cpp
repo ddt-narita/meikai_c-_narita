bea73a060188575697cc84d784bd047a8837e209
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:22:47 +0900

    修正コミット　演習02-09　（進捗率100%）作成者　成田修之

--- e_02_09/src/e_02_09.cpp
+++ e_02_09/src/e_02_09.cpp
@@ -1,19 +1,44 @@
-#include <iostream>				//main
-using namespace std;				//読み込んだ2つの整数の差が10以下かどうか表示
-									//
-int main(){						//5月1日
-	int a,b;						//成田修之
+/* 演習 02-09
+ * 読みこんだ2つの整数の差が10以下かどうかの表示
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新日：成田修之
+ */
 
-	cout <<"整数A：";	cin >>a;
-	cout <<"整数B：";	cin >>b;
 
-	//差sをa>bならa-bで、a<bならb-aで求める
-	int s = a > b ? a-b : b-a ;
+#include <iostream>
+using namespace std;
+int main()
+{
 
-	//求めた差が10以下か同課で判別する。
-	if (s<=10)
-		cout <<"それらの差は10以下です。\n";
-	else
-		cout <<"それらの差は11以上です。\n";
+	int nInputA;	//キーボードから入力し差を求めるための変数A
+	int nInputB;	//キーボードから入力し差を求めるための変数B
+	int nDif;		//AとBの差を求めて代入するのに使う変数
 
+	//変数Aに入力を促す表示
+	cout 	<< "整数A：";
+	//キーボードから入力
+	cin 	>> nInputA;
+	//変数Bに入力を促す表示
+	cout 	<< "整数B：";
+	//キーボードから入力
+	cin 	>> nInputB;
+
+	//差nDifをa>bならa-bで、a<bならb-aで求める
+	nDif = nInputA > nInputB ? nInputA - nInputB : nInputB -nInputA ;
+
+	//求めた差が10以下の時
+	if (nDif <= 10) {
+		//10以下であることを表示
+		cout << "それらの差は10以下です。\n";
+	}
+	//それ以外の場合
+	else {
+		//11以上であることを表示
+		cout << "それらの差は11以上です。\n";
+	}
+
+	//main関数の返却値0
+	return 0;
 }
