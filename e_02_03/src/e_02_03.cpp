01a26587df150e38f9fe24f91fe4dacdbeaac952
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:18:07 +0900

    修正コミット　演習02-03　（進捗率100％）作成者　成田修之

--- e_02_03/src/e_02_03.cpp
+++ e_02_03/src/e_02_03.cpp
@@ -1,22 +1,43 @@
-#include<iostream>						//main
-using namespace std;					//aとbの大小関係を求めて表示
-										//
-int main(){							//５月１日
-	int a;								//成田修之
-	int b;
+/* 演習 02-03
+ * 2つの整数の大小関係を求めて表示
+ * 作成日：5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之 */
 
-	//aとbに値を入力と表示
-	cout<<"整数a：";
-	cin >>a;
-	cout<<"整数b：";
-	cin >>b;
+#include<iostream>
+using namespace std;
 
-	//aとbの大小関係を表示
-	if (a>b)
-		cout <<"aのほうが大きいです。\n";
-	else if(a<b)
-		cout <<"bのほうが大きいです。\n";
-	else
-		cout <<"aとbは同じ値です。\n";
+int main()
+{
+	int nInput_a;	//キーボードから入力し整数bと比べるための変数a
+	int nInput_b;	//キーボードから入力し整数aと比べるための変数b
 
+	//整数bの値の入力を促す表示
+	cout << "整数a：";
+	//キーボードから入力
+	cin  >> nInput_a;
+	//整数aの値の入力を促す表示
+	cout << "整数b：";
+	//キーボードから入力
+	cin  >> nInput_b;
+
+	//整数aのほうが大きいとき
+	if (nInput_a > nInput_b) {
+		//aのほうが大きいことを表示
+		cout << "aのほうが大きいです。\n";
+
+	//整数bのほうが大きいとき
+	} else if(nInput_a < nInput_b) {
+		//bのほうが多いいことを表示
+		cout << "bのほうが大きいです。\n";
+
+	//それ以外のaとbが同じ値をとるとき
+	} else {
+		//aとbが同じ値であることを表示
+		cout << "aとbは同じ値です。\n";
+	}
+
+	//main関数の返却値0
+	return 0;
 }
