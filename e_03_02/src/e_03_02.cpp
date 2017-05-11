e7fa25836578646c7d274fe42326478243c6d157
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 14:29:57 +0900

    修正コミット　演習03-02　（進捗率100%）作成者　成田修之

--- e_03_02/src/e_03_02.cpp
+++ e_03_02/src/e_03_02.cpp
@@ -1,34 +1,56 @@
-#include <ctime>					//main
-#include <cstdlib>					//2桁のランダムな数の数当てゲーム
-#include <iostream>				//
-									//5月2日
-using namespace std;				//成田修之
+/* 演習03-02
+ * 2桁のランダムな数当てゲーム
+ * 作成日：5月2日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之
+ */
 
-int main(){
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
 	//ランダムな数を生成する種（基準）を設定
-	srand(time(NULL));
+	srand( time(NULL) );
 
 	//2桁の整数なので0～89の整数をランダムに作った後+10して10～99の整数にする
-	int no =rand()%90+10;
-	int x;
+	int nNumber = rand() % 90 + 10;
 
-	cout<<"数当てゲーム開始！\n";
-	cout<<"10～99の2桁の整数を当ててください\n";
+	int nInput;		//ランダムな値を予想し入力するための変数
+
+	//数当てゲームをこれから始めることの表示
+	cout << "数当てゲーム開始！\n";
+	//数当てゲームの説明の表示
+	cout << "10～99の2桁の整数を当ててください\n";
 
 	//正解するまで続ける
-	do{
-		cout<<"いくつかな？：";
-		cin>>x;
+	do {
+		//ランダムな値がいくつなのか入力することを促す表示
+		cout 	<< "いくつかな？：";
+		//キーボードから入力
+		cin 	>> nInput;
 
-		//入力した数とランダムに作られた2桁の数の関係に応じて表示
-		if(x>no)
-			cout<<"もっと小さな数だよ。\n";
-		else if (x<no)
-				cout <<"もっと大きい数だよ。\n";
+		//入力された値が大きいとき
+		if(nInput > nNumber) {
+			//ランダムな値が入力された値より小さいことを表示
+			cout << "もっと小さな数だよ。\n";
+
+		//入力された値が小さいとき
+		} else if (nInput < nNumber) {
+			//ランダムな値が入力された値より大きいことを表示
+			cout << "もっと大きい数だよ。\n";
 		}
-	//不正解なら繰り返す
-	while (x!=no);
 
-	cout<<"正解です！\n";
+	//不正解なら繰り返す
+	}while (nInput != nNumber);
+
+	//do文を抜けたら正解であると表示
+	cout << "正解です！\n";
+
+	//main関数の返却値0
+	return 0;
 }
