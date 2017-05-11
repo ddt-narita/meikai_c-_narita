055906f0c7ac03c4234a037686549694ba52e940
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 10:26:38 +0900

    修正コミット　演習02-14　（進捗率100%）作成者　成田修之

--- e_02_14/src/e_02_14.cpp
+++ e_02_14/src/e_02_14.cpp
@@ -1,21 +1,39 @@
-#include<ctime>						//main
-#include<cstdlib>						//グーチョキパーをランダムに表示
-#include<iostream>						//
-										//5月1日
-using namespace std;					//成田修之
+/* 演習 02-14
+ * グーチョキパーをランダムに表示
+ * 作成日；5月1日
+ * 作成者：成田修之
+ * 更新日：5月9日
+ * 更新者：成田修之 */
 
-int main(){
+//ランダムな値を設定する際に時刻を返却するtime関数を含むヘッダ
+#include<ctime>
+//rand関数を含むc標準ライブラリヘッダ
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int nHand;	//じゃんけんの手を決めるのに使う変数
+
+	//ランダムな値を生成する種を設定
 	srand(time(NULL));
 
-	//０、１，２の3つの中からランダムな数を生成
-	int hand=rand()%3;
+	//０、１，２の3つの中からランダムな数を生成して代入
+	nHand = rand() % 3;
 
-	//生成された値に応じたラベルへ飛ぶ
-	switch(hand){
-	case 0: cout <<"グー\n";		break;
-	case 1: cout <<"チョキ\n";		break;
-	case 2: cout <<"パー\n";		break;
-
+	//ランダムに生成されたnHandの値によって分岐
+	switch(nHand) {
+		//0の時グーを表示
+		case 0: cout << "グー\n";		break;
+		//1の時チョキを表示
+		case 1: cout << "チョキ\n";		break;
+		//2の時パーを表示
+		case 2: cout << "パー\n";		break;
 	}
+
+	//main関数の返却値0
+	return 0;
 
 }
