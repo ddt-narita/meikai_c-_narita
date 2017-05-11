fabc0f9ad4928c2bb81a78d5247c781a1503818c
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-11 14:33:59 +0900

    修正コミット　演習03-08　（進捗率100%）作成者　成田修之

--- e_03_08/src/e_03_08.cpp
+++ e_03_08/src/e_03_08.cpp
@@ -1,19 +1,38 @@
-#include<iostream>				//main
-using namespace std;			/*list3-10を読み込んだ値が1未満の時改行
-								文字を出力しないように変えてプログラムを作成*/
-								//
-int main(){					//5月2日
-								//成田修之
-	int n;
-	cout<<"何個*を表示しますか：";
-	cin>>n;
+/* 演習 03-08
+ * list03-10を読み込んだ値が1未満の時改行文字を出漁しないように変更
+ * 作成日：5月2日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
 
-	//for文を使ってiがnまで増えていく毎に*を表示しているのでn個の*が表示される
-	for(int i=0;i<n;i++)
-		cout<<'*';
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//個数の入力に使う変数
+	int nNumber;
+
+	//表示する個数の入力を促す表示
+	cout 	<< "何個*を表示しますか：";
+	//個数をキーボードから入力
+	cin		>> nNumber;
+
+	//一文字表示に関しては追加可能なようにダブルコーテーション
+
+	//iが0からnNumberまで増えていく毎に*(nNumber個)を表示
+	for(int i = 0; i < nNumber; i++) {
+		//アスタリスクを表示
+		cout << "*";
+	}
 
 	//nが1以上の時のみ改行文字を出力するようにする
-	if(n>=1)
-		cout<<'\n';
+	if(nNumber >= 1) {
+		cout << "\n";
+	}
 
+	//main関数の返却値0
+	return 0;
 }
