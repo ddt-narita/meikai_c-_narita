930ab2c70605225e88e249ab92086727aacee886
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:41:08 +0900

    修正コミット　演習05-01　（進捗率100%）作成者　成田修之

--- e_05_01/src/e_05_01.cpp
+++ e_05_01/src/e_05_01.cpp
@@ -1,24 +1,30 @@
-//演習０５－０１
-//int型の要素数5の配列の要素に先頭から順に5，4，3，2，1、を代入して表示
-//
-//5月5日
-//
+/*演習05-01
+ * int型の要素数5の配列の要素に先頭から順に5，4，3，2，1、を代入して表示
+ * 作成日：5月5日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ */
 #include<iostream>
 using namespace std;
 int main()
 {
-	const int nArrayNumber = 5;	//配列nArrayの要素の数(最初の提出時定数オブジェクト
-									//にしなくてもできてしまいそれで提出してしまった
 
-	int nArray[nArrayNumber];	//nArrayNumber個の要素を持つ配列nArray
+	const int nArrayNumber = 5;	//配列nArrayの要素の数(定数オブジェクト）
+	int nArray[nArrayNumber];		//nArrayNumber個の要素を持つ配列nArray
 
-	//各要素にnArrayNumberからカウントダウンして代入
+	//各要素にnArrayNumberからカウントダウン
 	for(int i = 0; i < nArrayNumber; i++) {
+		//各要素に代入
 		nArray[i] = nArrayNumber - i;
 	}
 
-	//各配列の要素を表示
+	//要素の個数分繰り返す
 	for(int i = 0; i< nArrayNumber; i++) {
+		//各要素を表示する
 		cout <<"nArray[" << i << "] = "<< nArray[i] << '\n';
 	}
+
+	//main関数の返却値
+	return 0;
 }
