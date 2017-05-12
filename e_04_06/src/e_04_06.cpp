2ffcc005b4f9ed92e5708ab7f85f4e862f5dafe7
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:16:10 +0900

    修正コミット　演習04-06　（進捗率100%）作成者　成田修之

--- e_04_06/src/e_04_06.cpp
+++ e_04_06/src/e_04_06.cpp
@@ -1,14 +1,24 @@
-//演習０４－０６
-//3種類の浮動小数点型の大きさを表示するプログラムの作成
-//
-//5月4日
-//成田修之
+/* 演習 04-06
+ * 3種類の浮動小数点型の」大きさを表示するプログラムの作成
+ * 作成日：5月4日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
+
 #include<iostream>
 using namespace std;
 int main()
 {
+	//大きさをそれぞれ表示
 	cout << "3種類の浮動小数点型の大きさを表示します。\n";
+	//float型
 	cout << "float型：" << sizeof(float) <<'\n';
+	//double型
 	cout << "double型：" << sizeof(double) << '\n';
+	//long double型
 	cout << "long double型"<< sizeof(long double) << '\n';
+
+	//main関数の返却値
+	return 0;
 }
