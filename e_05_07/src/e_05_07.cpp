d4bcf5ea82a35e48515c554f0ffd25c74272482e
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:45:32 +0900

    修正コミット　演習05-07　（進捗率100%）作成者　成田修之

--- e_05_07/src/e_05_07.cpp
+++ e_05_07/src/e_05_07.cpp
@@ -1,8 +1,11 @@
-//演習05－07
-//演習05－06を縦方向の棒グラフで表示、最下段には要素の添字を10で割った剰余を表示
-//
-//5月6日
-//成田修之
+/*演習05－07
+ * 演習05－06を縦方向の棒グラフで表示、最下段には要素の添字を10で割った剰余を表示
+ * 作成日：5月6日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ */
+
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
@@ -12,39 +15,48 @@ int main()
 {
 	//ランダムな値を作成する規準を設定
 	srand(time(NULL));
-	//配列nArrayの要素数
-	const int nArrayNumber = 15;
-	int nArray[nArrayNumber];
 
-	//0からいくつのランダムな値を代入するか
-	int nAstaMax = 10;
+	const int nArrayNumber = 15;	//配列nArrayの要素数
+	int nArray[nArrayNumber];		//int型の配列
+	int nAstaMax = 10;				//0からいくつのランダムな値を代入するか
 
-	//各要素に0～nAstaMaxのランダムな値を代入
+	//要素数分繰り返す
 	for(int i = 0; i < nArrayNumber; i++) {
+		//各要素にランダムな値を生成して代入
 		nArray[i] = rand() % (nAstaMax + 1);
 	}
 
-	//0～nAstaMax-1行、0～nArrayNumber-1列と考えて
+	//0から最大数まで繰り返す
 	for(int i = 0; i < nAstaMax; i++) {
+		//要素数分繰り返す
 		for(int j = 0; j < nArrayNumber; j++) {
 
-			//ランダムに代入した各要素の値が最大値からその列の値を引いた値以上なら
-			//アスタリスクを表示し、未満なら空白を表示
+			//ランダムに代入した各要素の値が最大値からその列の値を引いた値以上の時
 			if(nArray[j] >= (nAstaMax - i)) {
+				//アスタリスクを表示
 				cout << " *";
+			//未満の時
 			} else {
+				//空白文字を表示
 				cout << "  ";
 			}
 		}
+		//要素ごとに改行
 		cout << " \n";
 	}
-	//要素数の2倍'-'を表示
+	//要素数の2倍と1個'-'で区切りを表示
 	for(int i = 0; i < nArrayNumber * 2 + 1; i++) {
+		//-を表示する
 		cout << '-' ;
 	}
+	//ラベルをその下に書くよう改行
 	cout << "\n";
-	//要素の添字を10で割った剰余を表示
+	//要素数分繰り返す
 	for(int i = 0; i < nArrayNumber; i++) {
+		//一つ空白を挟んで10で割った剰余を表示する
 		cout << ' ' << i%10;
 	}
+
+	//main関数の返却値
+	return 0;
 }
