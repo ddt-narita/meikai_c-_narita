c173d46b9f389d94d28cdcb4b6e94fa70a2cca76
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:43:03 +0900

    修正コミット　演習05-04　（進捗率100%）作成者　成田修之

--- e_05_04/src/e_05_04.cpp
+++ e_05_04/src/e_05_04.cpp
@@ -1,8 +1,10 @@
-//演習０５－０４
-//演習０５－０３を連続する要素が同じ値をとらないように書き換える
-//
-//５月５日
-//成田修之
+/* 演習05-04
+ * 演習05-04を連続する要素が同じ値をとらないように書き換える
+ * 作成日：5月5日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ */
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
@@ -12,17 +14,22 @@ int main()
 	//ランダムな値を生成する基準を設定
 	srand(time(NULL));
 
-	//int型の配列nArrayの要素数
-	const int nArrayNumber = 6;
-	int nArray[nArrayNumber];
-	int n;
+	const int nArrayNumber = 6;		//int型の配列nArrayの要素数
+	int nArray[nArrayNumber] = {0};		//int型の配列
+	int nRand = 0;						//作成したランダムな値を代入するのに使う変数
 
-	//各要素に毎回ランダムな値を生成し代入
+	//要素数分繰り返す
 	for(int i =0; i < nArrayNumber; i++) {
-		for( ;n == nArray[i-1];) {
-			n =rand() % 10 +1;
-		}
-		nArray[i] = n;
+		//ひとつ前の要素と同じ値の限り続ける
+		do {
+			//ランダムな値を生成して代入
+			nRand =rand() % 10 +1;
+		}while(nRand == nArray[i-1]);
+		//各要素に代入
+		nArray[i] = nRand;
+		//各要素を表示
 		cout << "nArray[" << i << "] = " << nArray[i] << '\n';
 	}
+	//main関数の返却値
+	return 0;
 }
