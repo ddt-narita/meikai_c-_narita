b6001a52e1b8b45e984cb73d8123e2b1b6b67e30
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:46:30 +0900

    修正コミット　演習05-09　（進捗率100%）作成者　成田修之

--- e_05_09/src/e_05_09.cpp
+++ e_05_09/src/e_05_09.cpp
@@ -1,8 +1,11 @@
-//演習05－09
-//配列aの全要素を配列bに逆順にコピーする
-//
-//5月6日
-//成田修之
+/*演習05－09
+ * 配列aの全要素を配列bに逆順にコピーする
+ * 作成日：5月6日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ */
+
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
@@ -13,22 +16,28 @@ int main ()
 	srand(time(NULL));
 	//配列の要素数
 	const int nArrayNumber = 5;
-	//配列aとｂ
-	int nArray[nArrayNumber];
-	int nArrayCopy[nArrayNumber];
 
-	//nArray_1にランダムな数を代入して表示
+	int nArray[nArrayNumber];		//int型の配列
+	int nArrayCopy[nArrayNumber];	//int型の上の配列のコピー配列
+
+	//要素数分繰り返す
 	for(int i=0; i < nArrayNumber; i++) {
+		//各要素にランダムな値を代入
 		nArray[i] = rand() % 10;
+		//各要素を表示
 		cout<<"nArray_1[" << i <<"] = " << nArray[i] << "\n";
 	}
+	//これから逆順にコピーすることの明示
 	cout << "配列を逆順にコピーします。\n";
 
-	//配列を逆順にコピーする nArray_2[nArrayNumber-1]にはnArray_1[i]といったように
+	//要素数分繰り返す
 	for(int i = 0; i<nArrayNumber; i++) {
-		nArrayCopy[i] = nArray[nArrayNumber- i -1];
+		//配列を逆順にコピーする nArray_2[nArrayNumber-1]にはnArray_1[i]といったように
+		nArrayCopy[i] = nArray[nArrayNumber - i - 1];
 		//コピーしたものを表示
 		cout << "nArrayCopy[" << i << "] = " << nArrayCopy[i] << "\n";
 	}
+	//main関数の返却値
+	return 0;
 
 }
