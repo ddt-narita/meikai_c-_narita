ea5622c9d11eeb661b4d94e8987ddb1bd047a684
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:46:00 +0900

    修正コミット　演習05-08　（進捗率100%）作成者　成田修之

--- e_05_08/src/e_05_08.cpp
+++ e_05_08/src/e_05_08.cpp
@@ -1,35 +1,50 @@
-//演習05－08
-//配列の要素の並びをシャッフルする
-//
-//5月6日
-//成田修之
+/* 演習05－08
+ * 配列の要素の並びをシャッフルする
+ * 作成日：5月6日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ */
+
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
 using namespace std;
 int main()
 {
+	//ランダムな値を生成する種を設定
 	srand(time(NULL));
-	const int nArrayNumber =7; //要素数
-	int nArray[nArrayNumber];
 
-	//各要素にランダムな値を代入して表示
+	const int nArrayNumber =7; //要素数
+	int nArray[nArrayNumber];	//int型の配列
+
+	//要素数分繰り替えす
 	for(int i = 0; i < nArrayNumber; i++) {
+		//各要素にランダムな値を代入
 		nArray[i] = rand() %100;
+		//各要素を表示
 		cout<<"nArray[" << i << "] = " << nArray[i] <<'\n';
 	}
+	//シャッフルすることを明示
 	cout << "要素をランダムにシャッフルします\n";
 
-	//ランダムな数を生成し、i番目の要素と生成した数番目の数を入れ替える
+	//要素数分繰り返す
 	for(int i = 0; i < nArrayNumber; i++) {
+		//各要素をどこと入れ替えるかランダムな値を生成し代入
 		int nRucky = rand() % nArrayNumber;
-		int t = nArray[i];
+
+		//その時のiの要素を避難させる
+		int nTrade = nArray[i];
+		//iに生成したランダムな値個目の要素の値を代入
 		nArray[i] = nArray[nRucky];
-		nArray[nRucky] = t;
+		//避難させていたiの要素をランダムな値個目に代入する
+		nArray[nRucky] = nTrade;
 	}
-	//入れ替えた各要素を表示
+	//要素数分繰り返す
 	for(int i = 0; i < nArrayNumber; i++) {
+		//各要素を表示
 		cout<<"nArray[" << i << "] = " << nArray[i] <<'\n';
 	}
+	//main関数の返却値
 	return 0;
 }
