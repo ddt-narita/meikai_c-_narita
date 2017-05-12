2164bd909cb3c57a4d52a2ed37945deee7f83f5c
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:19:00 +0900

    修正コミット　演習04-12　（進捗率100%）作成者　成田修之

--- e_04_12/src/e_04_12.cpp
+++ e_04_12/src/e_04_12.cpp
@@ -1,8 +1,10 @@
-//演習０４－１２
-//トランプの記号の4つの列挙子をを持つ列挙帯を定義して入力した値を読み込んで対応した記号を表示
-//
-//5月5日
-//成田修之
+/* 演習０４－１２
+ * トランプの記号の4つの列挙子をを持つ列挙帯を定義して入力した値を読み込んで対応した記号を表示
+ * 作成日：5月5日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ * */
 #include <iostream>
 using namespace std;
 int main()
@@ -13,22 +15,32 @@ int main()
 	//入力に使う変数　for文を最初に入っていくために0～4以外の数で初期化
 	int nInputTrump = -1;
 
+	//0から4の値以外が入力される限り繰り返す
 	for( ; nInputTrump < Dia || nInputTrump > Invalid; ) {
+		//入力を促す表示
 		cout << "トランプの記号を表示します。0…ダイア、1…ハート、2…クラブ、"
 				"3…スペード、4…終了：";
+		//キーボードから入力
 		cin >> nInputTrump;
 	}
 
 	//Invalidは終了。他はその記号を表示
 	if(nInputTrump != Invalid) {
 		trump selected =static_cast<trump>(nInputTrump); //int型で入れた0～4の値をtrump型の割り振られたものに変更
+		//入力された値によって分岐
 		switch(selected) {
+		//0（ダイア）の時
 		case Dia: cout << "♦ダイヤ\n";		break;
+		//1（ハートの時）
 		case Heart: cout << "💛ハート\n";		break;
+		//2（クラブの時）
 		case Club: cout << "♣クラブ\n";		break;
+		//3（スペードの時）
 		case Spade: cout << "♠スペード\n";	break;
 		}
 	}
+	//main関数の返却値0
+	return 0;
 }
 /*for文の条件でnInputTrump<0||nInputTrump>4でも実行できることが確認できた
 またselectedを宣言せずswitch文の条件部をnInputTrumpに変えても作動した
