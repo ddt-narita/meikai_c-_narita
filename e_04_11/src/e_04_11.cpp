c1fc6f8b72aaa6cae6f539d86de9759f03f0f153
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:18:28 +0900

    修正コミット　演習04-11　（進捗率100%）作成者　成田修之

--- e_04_11/src/e_04_11.cpp
+++ e_04_11/src/e_04_11.cpp
@@ -1,8 +1,12 @@
-//演習０４－１１
-//0.0～1.0まで0.001ずつ増やしながらその値とその値の2乗を表示する
-//
-//5月5日
-//成田修之
+/* 演習 04-11
+ * 0.0～1.0まで0.001ずつ増やしながらその値とその値の2乗を表示する
+ * 作成日：5月5日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者；成田修之
+ */
+
+//操作子を使うのに必要なヘッダ
 #include<iomanip>
 #include<iostream>
 using namespace std;
@@ -18,12 +22,15 @@ int main()
 		int nPower = 2;
 
 		//iのべき乗に使う変数
-		float fiPower = 1;
+		float floPower = 1;
 
 		//n回fiPowerにiをかけたものを代入、jはカウント
 		for (int j = 1; j <= nPower; j++) {
-			fiPower *= i;
+			floPower *= i;
 		}
-		cout << fiPower << '\n';
+		//べき乗したものを表示
+		cout << floPower << '\n';
 	}
+	//main関数の返却値
+	return 0;
 }
