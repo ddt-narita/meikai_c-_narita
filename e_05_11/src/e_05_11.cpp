8dc830ef07e7c0b4f671e51e929a33a5845b292c
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:47:33 +0900

    修正コミット　演習05-11　（進捗率100%）作成者　成田修之　

--- e_05_11/src/e_05_11.cpp
+++ e_05_11/src/e_05_11.cpp
@@ -1,67 +1,103 @@
-//演習05－11
-//6人2科目の点数を読み込んで科目ごとの合計点と平均点、学生ごとの合計点と平均を求める
-//
-//5月11日
-//成田修之
+/*演習05－11
+ * 6人2科目の点数を読み込んで科目ごとの合計点と平均点、学生ごとの合計点と平均を求める
+ * 作成日：5月11日
+ * 作成者：成田修之
+ */
+
 #include<iostream>
 using namespace std;
-int main () {
-
+int main ()
+{
+	//学年の数
+	const int nGrade =2;
 	//生徒の数
 	const int nStudentNumber = 6;
 	//科目数
 	const int nSubNumber = 2;
-	//[生徒数][科目数]の2次配列
-	int nArray[nStudentNumber][nSubNumber];
+	//[学年][生徒数][科目数]の3次配列
+	int nArray[nGrade][nStudentNumber][nSubNumber];
 
-	//各要素に入力する
-	for(int i =0; i < nStudentNumber; i++) {
-		for(int j =0; j < nSubNumber; j++) {
 
-			//jの剰余を使って国語と数学を交互に入力する
-			if(j % 2 == 0){
-				cout << i+1 << "番目の人の国語の点数：";
-				cin >> nArray[i][j];
-			} else if(j % 2 == 1){
-				cout << i+1 << "番目の人の数学の点数：";
-				cin >> nArray[i][j];
+	//学年の数だけ繰り返す
+	for(int i =0; i < nGrade; i++) {
+		//生徒数だけ繰り返す
+		for(int j =0; j <  nStudentNumber; j++) {
+			//科目の数だけ繰り返す
+			for(int k = 0; k <nSubNumber; k++) {
+				//kの剰余を使って国語と数学を交互に入力する
+				if(k % 2 == 0){
+					//国語の点数の入力を促す
+					cout << i + 1 << "学年" << j+1 << "番目の人の国語の点数：";
+					//キーボードから入力
+					cin >> nArray[i][j][k];
+					//剰余が1の時
+				} else {
+					//数学の点数の入力を促す表示
+					cout << i + 1 << "学年" << j+1 << "番目の人の数学の点数：";
+					//キーボードから入力
+					cin >> nArray[i][j][k];
+				}
 			}
 		}
 	}
 
-	//科目ごとの合計
-	int nSumJap=0;
-	int nSumMath=0;
-	//科目ごとの合計を求める
-	for(int i = 0; i < nStudentNumber; i++) {
-		for(int j = 0; j < nSubNumber; j++) {
-			//jの剰余をつかって科目ごとの合計を出す
-			if(j % 2 == 0){
-				nSumJap += nArray[i][j];
-			} else if (j % 2 ==1){
-				nSumMath += nArray[i][j];
+	int nSumJap=0;		//国語の点の合計
+	int nSumMath=0;		//数学の点の合計
+
+
+	//学年分繰り返す
+	for(int i = 0; i < nGrade; i++) {
+
+		nSumJap=0;		//国語の点の合計を学年ごとにリセット
+		nSumMath=0;		//数学の点の合計を学年ごとにリセット
+
+		//生徒数分繰り返す
+		for(int j = 0; j <nStudentNumber; j++) {
+			//科目数分繰り返す
+			for(int k =0; k < nSubNumber; k++){
+				//jの剰余をつかって科目ごとの合計を出す
+				if(k % 2 == 0){
+					//国語の合計に国語の点が入力されている配列の要素を加算して代入
+					nSumJap += nArray[i][j][k];
+					//剰余が1の時
+				} else {
+					//数学の点の合計に数学の点が入力されている配列の要素を加算して代入
+					nSumMath += nArray[i][j][k];
+				}
 			}
 		}
+
+		/*平均は合計を生徒数で割ったものnSumはどちらもint型なのでdoule型にキャストする
+		 *また学年ごとで合計を使うためこの位置で宣言
+		 */
+		double nAveJap = static_cast<double>(nSumJap) / nStudentNumber;
+		double nAveMath = static_cast<double>(nSumMath) / nStudentNumber;
+
+		//i学年の国語について表示
+		cout << i + 1 << "学年の国語の合計点は：" << nSumJap	<< "\n平均点は：" << nAveJap <<"\n";
+		//i学年数学について表示
+		cout << i + 1 << "学年の数学の合計点は：" << nSumMath	<< "\n平均点は：" << nAveMath <<"\n";
 	}
 
-	//平均は合計を生徒数で割ったものnSumはどちらもint型なのでdoule型にキャストする
-	double nAveJap = static_cast<double>(nSumJap) / nStudentNumber;
-	double nAveMath = static_cast<double>(nSumMath) / nStudentNumber;
-	cout << "国語の合計点は：" << nSumJap	<< "\n平均点は：" << nAveJap <<"\n";
-	cout << "数学の合計点は：" << nSumMath	<< "\n平均点は：" << nAveMath <<"\n";
+	//各生徒の合計の配列
+	int nSumStudent[nGrade][nStudentNumber];
+	//各生徒の平均の配列
+	double nAveStudent[nGrade][nStudentNumber];
 
-	//各生徒の合計と平均（6人分）の配列
-	int nSumStudent[nStudentNumber];
-	double nAveStudent[nStudentNumber];
 
-	for(int i =0; i < nStudentNumber; i++) {
-
-		//生徒ごとの合計は同じ行ごとの合計
-		nSumStudent[i] = nArray[i][0] + nArray[i][1];
-		//生徒ごとの平均（合計はint型なのでdouble型にキャスト
-		nAveStudent[i] = static_cast<double>(nSumStudent[i]) / nSubNumber;
-		//合計と平均を表示
-		cout << i <<"番目の生徒合計は：" << nSumStudent[i] << "\n" <<"平均は："<< nAveStudent[i] << "\n";
-
+	//学年数分繰り返す
+	for(int i =0; i < nGrade; i++) {
+		//生徒数分繰り返す
+		for(int j = 0; j < nStudentNumber; j++) {
+			//生徒ごとの合計は同じ行ごとの合計
+			nSumStudent[i][j] = nArray[i][j][0] + nArray[i][j][1];
+			//生徒ごとの平均（合計はint型なのでdouble型にキャスト
+			nAveStudent[i][j] = static_cast<double>(nSumStudent[i][j]) / nSubNumber;
+			//合計と平均を表示
+			cout << i + 1 << "学年" << j + 1 <<"番目の生徒合計は：" << nSumStudent[i][j] << "\n";
+			cout << "平均は："	<< nAveStudent[i][j] << "\n";
+		}
 	}
+	//main関数の返却値
+	return 0;
 }
