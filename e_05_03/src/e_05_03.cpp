c43ad820364803d5f9f872aa35738e5eefeb54c0
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:42:33 +0900

    修正コミット　演習05-03　（進捗率100%）作成者　成田修之

--- e_05_03/src/e_05_03.cpp
+++ e_05_03/src/e_05_03.cpp
@@ -1,8 +1,10 @@
-//Exercise 05-03
-//int型で要素数６の配列に１～１０のランダムな値を代入する
-//
-//５月５日
-//成田修之
+/* 演習05-03
+ * int型で要素数６の配列に１～１０のランダムな値を代入する
+ * 作成日：5月5日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 更新者：成田修之
+ * */
 #include<ctime>
 #include<cstdlib>
 #include<iostream>
@@ -12,13 +14,17 @@ int main()
 	//ランダムな値を生成する基準を設定
 	srand(time(NULL));
 
-	//int型の配列nArrayの要素数
-	const int nArrayNumber = 6;
-	int nArray[nArrayNumber];
+	const int nArrayNumber = 6;	//int型の配列nArrayの要素数
+	int nArray[nArrayNumber];		//int型の配列
 
-	//各要素に毎回ランダムな値を生成し代入
+	//要素数分繰り返す
 	for(int i =0; i < nArrayNumber; i++) {
+		//各要素にランダムな値を代入
 		nArray[i] = rand() % 10 + 1;
+		//各要素を表示
 		cout << "nArray[" << i <<"] = " <<nArray[i] << '\n';
 	}
+
+	//main関数の返却値
+	return 0;
 }
