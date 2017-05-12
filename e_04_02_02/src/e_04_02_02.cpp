1ed92b93549776cb20e3d7960b1cb0fc2e5fa507
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-12 18:14:02 +0900

    修正コミット　演習04-02　（進捗率100%）作成者　成田修之

--- e_04_02_02/src/e_04_02_02.cpp
+++ e_04_02_02/src/e_04_02_02.cpp
@@ -1,10 +1,17 @@
-//演習０４－０２
-//unsigned型の全数値を8進数、10進数、16進数で表示
-//
-//5月4日
-//成田修之
+/* 演習 04-02-02
+ * unsigned型の全数値を8進数、10進数、16進数で表示
+ * 作成日：5月4日
+ * 作成者：成田修之
+ * 更新日：5月10日
+ * 更新者：成田修之
+ */
+//型の最小値最大値を含んだヘッダ
 #include<climits>
+//操作子を使うためのヘッダ
+#include<iomanip>
 #include<iostream>
+//基からのUINT_MAXでは大きすぎるので自分で再度設定
+#define UINT_MAX 510
 using namespace std;
 
 int main()
@@ -18,9 +25,14 @@ int main()
 		//16進数
 		cout << hex << i << ' ';
 
+		//unsign型の最大値と等しくなったら終了
 		if(i == UINT_MAX) {
 			break;
 		}
+	//10進数で1進むごとに改行する
 	cout << '\n';
 	}
+
+	//main型の返却値
+	return 0;
 }
