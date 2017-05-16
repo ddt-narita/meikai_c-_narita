baf1a4364d1e958939fc7b415d549afe92961088
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-16 16:30:46 +0900

    新規コミット　演習08-01　（進捗率100%）作成者　成田修之

---
+++ e_08_01/src/e_08_01.cpp
@@ -0,0 +1,18 @@
+/* 演習08－01
+ * list8-4の配列の宣言を書き換え
+ * 作成日：5月16日
+ * 作成者：成田修之
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//char
+	char s[] = "ABC\0DEF";
+	//
+	cout << "配列sに文字列\"" << s << "\"が格納されています\n";
+
+	return 0;
+}
