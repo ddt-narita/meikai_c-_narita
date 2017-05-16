451f97e562ef82ffdb3c452d92de4ec920e3d108
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-16 10:35:06 +0900

    修正コミット　演習06-12　（進捗率１００％）作成者　成田修之

--- e_06_12/src/e_06_12.cpp
+++ e_06_12/src/e_06_12.cpp
@@ -14,6 +14,34 @@ using namespace std;
  * 作成日：5月11日
  * 作成者：成田修之
  */
+int sum(int a,int b = 1);
+
+int main()
+{
+	int nInputA;	//整数Aを入力するために使う変数
+	int nInputB;	//整数Bを入力するために使う変数
+
+	//全整数の和を求めることを明示
+	cout	<< "整数Bから整数Aまでの全整数の和と1から整数Aまでの合計を求めます\n";
+	//整数Aの入力を促す表示
+	cout 	<< "整数A：";
+	//キーボードから入力
+	cin		>> nInputA;
+	//整数Bの入力を促す表示
+	cout	<< "整数B：";
+	//キーボードから入力
+	cin		>> nInputB;
+
+
+	//合計を表示
+	cout << nInputB << "から" << nInputA << "までの合計は" << sum(nInputA,nInputB) << "\n";
+	//1から整数Aまでの合計を表示
+	cout << "1から" << nInputA << "までの合計は" << sum(nInputA);
+
+	//main関数の返却値
+	return 0;
+}
+
 
 int sum(int a,int b = 1)
 {
@@ -26,27 +54,4 @@ int sum(int a,int b = 1)
 	}
 	//関数sumの返却値
 	return nSum;
-}
-
-int main()
-{
-	int nInputA;	//整数Aを入力するために使う変数
-	int nInputB;	//整数Bを入力するために使う変数
-
-	//全整数の和を求めることを明示
-	cout	<< "整数Bから整数Aまでの全整数の和を求めます\n";
-	//整数Aの入力を促す表示
-	cout 	<< "整数A：";
-	//キーボードから入力
-	cin		>> nInputA;
-	//整数Bの入力を促す表示
-	cout	<< "整数B：";
-	//キーボードから入力
-	cin		>> nInputB;
-
-	//合計を表示
-	cout << nInputB << "から" << nInputA << "までの合計は" << sum(nInputA,nInputB);
-
-	//main関数の返却値
-	return 0;
 }
