8abc4b529dd95c7fa6302a4d500ef3f6c021d60f
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-16 10:07:09 +0900

    修正コミット　演習06-02　（進捗率100%）作成者　成田修之

--- e_06_02/src/e_06_02.cpp
+++ e_06_02/src/e_06_02.cpp
@@ -1,36 +1,66 @@
-//演習０６－０２
-//3つのint型引数の最小値を求める関数の作成
-//
-//5月8日
-//成田修之
+/*演習06-02
+ * 3つのint型引数の最小値を求める関数の作成
+ * 作成日：5月8日
+ * 作成者：成田修之
+ * 更新日：5月11日
+ * 成田修之
+ */
+
 #include<iostream>
 using namespace std;
-int min (int nInput_1,int nInput_2,int nInput_3)
+
+/* min関数
+ * int型の3つ引数の最小値を求める関数
+ * 引数はint型で3つの数
+ * 返却値は最小値の入った変数nMax
+ * 作成日：5月8日
+ * 作成者：成田修之
+ */
+
+int min (int nInputA,int nInputB,int nInputC)
 {
-	int nMin =nInput_1;
-	if(nMin > nInput_2) {
-		nMin = nInput_2;
+	//返却値であり最小値nMinをnInputAで初期化
+	int nMin =nInputA;
+	//nMinよりBのほうが小さいとき
+	if(nMin > nInputB) {
+		//BをnMinに代入
+		nMin = nInputB;
 	}
-	if(nMin > nInput_3) {
-		nMin = nInput_3;
+
+	//nMinよりCのほうが小さいとき
+	if(nMin > nInputC) {
+		//CをnMinに代入
+		nMin = nInputC;
 	}
+
+	//返却値nMinを返す
 	return nMin;
 }
 
 int main()
 {
-	int nInput_1;
-	int nInput_2;
-	int nInput_3;
+	int nInputA;	//判定する数を入力するための変数A
+	int nInputB;	//判定する数を入力するための変数B
+	int nInputC;	//判定する数を入力するための変数C
+
+	//最小値を求めることの明示
 	cout << "最小値を求めます。\n";
+	//整数Aに入力を促す
 	cout << "整数A：";
-	cin >> nInput_1;
+	//キーボードから入力
+	cin  >> nInputA;
+	//整数Bに入力を促す
 	cout << "整数B：";
-	cin >> nInput_2;
+	//キーボードから入力
+	cin  >> nInputB;
+	//整数Cに入力を促す
 	cout << "整数C：";
-	cin >> nInput_3;
+	//キーボードから入力
+	cin  >> nInputC;
 
-	int nMin = min(nInput_1,nInput_2,nInput_3);
-	cout << "最小値は：" << nMin;
+	//最小値を表示
+	cout << "最小値は：" << min(nInputA,nInputB,nInputC);
 
+	//main関数の返却値
+	return 0;
 }
