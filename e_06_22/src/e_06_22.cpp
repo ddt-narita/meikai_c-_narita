6524244cef09d988c9e742050dc4eef3e12ac972
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-16 10:36:56 +0900

    修正コミット　演習06-22　（進捗率１００％）作成者　成田修之

--- e_06_22/src/e_06_22.cpp
+++ e_06_22/src/e_06_22.cpp
@@ -15,10 +15,8 @@ using namespace std;
  * 作成者：成田修之
  */
 
-inline double square(double dblInput)
-{
-	return dblInput * dblInput;
-}
+inline double square(double dblInput);
+
 
 /* 関数cube
  * 3条を求める
@@ -28,19 +26,33 @@ inline double square(double dblInput)
  * 作成者：成田修之
  */
 
-inline double cube(double dblInput)
-{
-	return dblInput * dblInput * dblInput;
-}
+inline double cube(double dblInput);
+
 
 int main()
 {
+	//2乗3乗する実数を入力するために使う変数
 	double dblInput;
 
+	//入力を促す表示
 	cout << "2乗と3乗を求めます。入力してください：";
+	//キーボードから入力
 	cin  >> dblInput;
 
+	//2乗3乗したものを表示
 	cout << "2乗は" << square(dblInput) << "3乗は" << cube(dblInput);
 
+	//main関数の返却値
 	return 0;
+}
+
+
+inline double square(double dblInput)
+{
+	return dblInput * dblInput;
+}
+
+inline double cube(double dblInput)
+{
+	return dblInput * dblInput * dblInput;
 }
