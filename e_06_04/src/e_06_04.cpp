315bdc16b73e0351a12b8b6b5b2714efa40409d5
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-16 10:07:36 +0900

    修正コミット　演習06-04　（進捗率100%）作成者　成田修之

--- e_06_04/src/e_06_04.cpp
+++ e_06_04/src/e_06_04.cpp
@@ -1,6 +1,6 @@
-/*演習０６－０４
+/*演習06-04
  * 半径ｒの円の面積を求めて返却する関数circ_areaを作成 （円周率は3.14)
- * 作成日：５月９日
+ * 作成日：5月9日
  * 作成者：成田修之
  */
 #include<iostream>
@@ -10,19 +10,10 @@ using namespace std;
  * 半径rの円の面積を求めて返却する
  * 引数はint型整数
  * 返却値はdouble型の面積
- * 作成日：５月９日
+ * 作成日：5月9日
  * 作成者：成田修之
  */
-double circ_area(int nRadius)
-{
-	//πは定数なので定数オブジェクトとして宣言し初期化
-	const double dblPI = 3.14;
-	//面積を求める（半径＊半径＊π）
-	double dblCirc_Area = nRadius * nRadius * dblPI;
-
-	//返却値dblCirc_Areaを返却
-	return dblCirc_Area;
-}
+double circ_area(int nRadius);
 
 int main()
 {
@@ -36,5 +27,18 @@ int main()
 
 	//面積を表示
 	cout << "半径" << nRadius << "の面積は：" << circ_area(nRadius) << "です。\n";
+
+	//main関数の返却値
+	return 0;
 }
 
+double circ_area(int nRadius)
+{
+	//πは定数なので定数オブジェクトとして宣言し初期化
+	const double dblPI = 3.14;
+	//面積を求める（半径＊半径＊π）
+	double dblCirc_Area = nRadius * nRadius * dblPI;
+
+	//返却値dblCirc_Areaを返却
+	return dblCirc_Area;
+}
