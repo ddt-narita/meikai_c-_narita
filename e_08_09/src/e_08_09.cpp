3991f7e7a53eedd657c01a476ad1a930288e7cf0
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-22 11:32:09 +0900

    修正コミット　演習08-09　（進捗率100%）作成者　成田修之

--- e_08_09/src/e_08_09.cpp
+++ e_08_09/src/e_08_09.cpp
@@ -16,6 +16,61 @@ using namespace std;
  * 作成者：成田修之
  */
 
+void strcpy2(char* s, const char* c);
+
+/* 演習08-09
+ * strncpyと等しい関数
+ * 引数は2つのchar型へのポインタ,int型変数
+ * 返却値は無し
+ * 作成日：5月16日
+ * 作成者：成田修之
+ */
+void strncpy2(char* s, const char* c, int n);
+
+
+int main()
+{
+	char chrStrA[35];				//オリジナル側の文字列
+	char chrStrB[35];				//コピーされる側の文字列
+	char x[43] ="XXXXXXXXXX";		//リセットするための文字列
+	int nInput;			//いくつ追加するか入力に使う
+
+	//入力を促す
+	cout << "文字列を入力してください：";
+	//キーボードから入力
+	cin  >> chrStrA;
+
+	//コピーすることを明示
+	cout << "文字列をコピーして表示します：";
+
+	//関数strcpy2を呼び出してコピー
+	strcpy2(chrStrB,chrStrA);
+
+	//コピーした文字列を表示
+	cout << chrStrB << "\n";
+
+	//文字列xでリセット
+	cout << "全要素をXで埋めました\n";
+	//関数strcpy2を呼び出してXで埋める
+	strcpy2(chrStrB,x);
+
+	//コピーする範囲を決める
+	cout<<"もう一度コピーします。どこまでコピーしますか：";
+	//キーボードから入力
+	cin >> nInput;
+
+	//関数strncpy2を呼び出して文字列bに文字列Aをｎ個コピーする
+	strncpy2(chrStrB,chrStrA,nInput);
+
+	//コピーした文字列を表示する
+	cout<< "表示します：";
+	cout << chrStrB;
+
+	//main関数の返却値
+	return 0;
+}
+
+
 void strcpy2(char* s, const char* c)
 {
 	//ナル文字になるまで繰り返す
@@ -25,21 +80,15 @@ void strcpy2(char* s, const char* c)
 	}
 }
 
-/* 演習08-09
- * strncpyと等しい関数
- * 引数は2つのchar型へのポインタ,int型変数
- * 返却値は無し
- * 作成日：5月16日
- * 作成者：成田修之
- */
-void strncpy2(char* s, const char* c,unsigned int n)
+
+void strncpy2(char* s, const char* c, int n)
 {
 	//長さはunsighed型なのでキャストしておく
-	int lenghs = static_cast<int>(strlen(c));
+	int length = strlen(c);
 	//コピーしたいものが入力された値より小さいとき
-	if(lenghs < n){
+	if(length < n){
 		//その長さ分だけコピー
-		for(unsigned int i = 0; i < lenghs; i++) {
+		for(int i = 0; i < length; i++) {
 			//コピーする
 			s[i]=c[i];
 		}
@@ -55,32 +104,4 @@ void strncpy2(char* s, const char* c,unsigned int n)
 		}
 	}
 
-}
-
-int main()
-{
-	char chrStrA[35];				//オリジナル側の文字列
-	char chrStrB[35];				//コピーされる側の文字列
-	char x[43] ="XXXXXXXXXX";		//
-	unsigned int nInput;			//
-
-	cout << "文字列を入力してください：";
-	cin  >> chrStrA;
-
-	cout << "文字列をコピーして表示します：";
-
-	strcpy2(chrStrB,chrStrA);
-
-	cout << chrStrB << "\n";
-
-	cout << "全要素をXで埋めました\n";
-	strcpy2(chrStrB,x);
-
-	cout<<"もう一度コピーします。どこまでコピーしますか：";
-	cin >> nInput;
-
-	strncpy2(chrStrB,chrStrA,nInput);
-
-	cout<< "表示します：";
-	cout << chrStrB;
 }
