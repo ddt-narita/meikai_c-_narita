ede4f201c96d604b9a341502395dfb12a967f88e
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-26 18:09:19 +0900

    修正コミット　演習13-01　（進捗率100%）作成者　成田修之

--- e_13_01/src/e_13_01.h
+++ e_13_01/src/e_13_01.h
@@ -79,6 +79,11 @@ public:
 		d = d2;
 	}
 
+	int days_of_month2 (int x, int y) const {
+		int a = days_of_month(x,y);
+		return a;
+	}
+
 	//2つのオブジェクトのすべてのデータメンバを比較して一致するかbool型で返却
 	friend bool operator== (const Date& x, const Date& y) {
 		return x.y == y.y&& x.m == y.m&& x.d == y.d;
@@ -90,245 +95,39 @@ public:
 	}
 
 	//後者のオブジェクトのほうが大きいかをbool型で返却する演算子関数
-	friend bool operator< (const Date& x, const Date& y) {
-		int nReturn;	//返却値
-		//年がｙのほうが大きいとき
-		if(x.y < y.y){
-			nReturn = 1;
-		//年がyのほうが小さいとき
-		} else if(x.y > y.y) {
-			nReturn = 0;
-		//年が同じとき
-		} else {
-			//月がyのほうが大きいとき
-			if(x.m < y.m){
-				nReturn = 1;
-			//月がyのほうが小さいとき
-			} else if(x.m > y.m) {
-				nReturn = 0;
-			//月も同じとき
-			} else {
-				//日がyのほうが大きいとき
-				if(x.d < y.d){
-					nReturn = 1;
-				//日まで同じかyのほうが小さいとき
-				} else {
-					nReturn = 0;
-				}
-			}
-		}
-		//bool型にキャストして返却
-		return static_cast<bool>(nReturn);
-	}
+	friend bool operator< (const Date& x, const Date& y);
 
+	//後者のオブジェクトのほうが小さいかをbool型で返却する演算子関数
+	friend bool operator> (const Date& x, const Date& y);
 
-	friend bool operator> (const Date& x, const Date& y) {
-		int nReturn;	//返却値
-		//年がxのほうが大きいとき
-		if(x.y > y.y){
-			nReturn = 1;
-			//年がxのほうが小さいとき
-		} else if(x.y < y.y) {
-			nReturn = 0;
-			//年が同じとき
-		} else {
-			//月がxのほうが大きいとき
-			if(x.m > y.m){
-				nReturn = 1;
-				//月がxのほうが小さいとき
-			} else if(x.m < y.m) {
-				nReturn = 0;
-				//月も同じとき
-			} else {
-				//日がxのほうが大きいとき
-				if(x.d > y.d){
-					nReturn = 1;
-					//日まで同じかxのほうが小さいとき
-					} else {
-						nReturn = 0;
-					}
-			}
-		}
-		//bool型にキャストして返却
-		return static_cast<bool>(nReturn);
-	}
 
 	//上で定義した2つを使ってどちらかを満たすとき真をbool型で返却
-	friend bool operator<= (const Date& x, const Date& y) {
-		return x < y || x == y;
-	}
+	friend bool operator<= (const Date& x, const Date& y);
 
 	//上で定義した2つを使ってどちらかを満たすとき真をbool型で返却
-	friend bool operator>= (const Date& x, const Date& y) {
-		return x > y || x == y;
-	}
+	friend bool operator>= (const Date& x, const Date& y);
 
 	//2つの日付の差、離れている日数を返す関数
-	friend int operator-(const Date& x, const Date& y) {
-		//返却する離れている日数
-		int dayDif = 0;
-		//大きいほう
-		int y1 = x > y ? x.y : y.y;
-		int m1 = x > y ? x.m : y.m;
-		//小さいほう
-		int y2 = x < y ? x.y : y.y;
-		int m2 = x < y ? x.m : y.m;
-		int d2 = x < y ?  y.d - x.d : x.d - y.d;
-
-		//同値の時0を返却
-		if(x == y) {
-			dayDif = 0;
-		//同値ではない時
-		} else  {
-			//小さいほうの日付が大きいほうと年と月が一致するまで繰り返す
-			while(y2 < y1 || m2 < m1) {
-				//その月の最大の日を足して代入
-				dayDif += days_of_month(y2,m2);
-				//月が12月の時1月に戻して1年進める
-				if(m2 == 12) {
-					m2 = 1;
-					y2++;
-				//月が12月以外はそのまま1増やす
-				} else {
-					m2++;
-				}
-			}
-			//日の差を足して日にちを調節する
-			dayDif += d2;
-		}
-
-		//yのほうが大きいとき負の値にして返す仕様なのでー1掛ける
-		if(x < y) {
-			dayDif *= -1;
-		}
-
-		//求めた離れている日数を返却する
-		return dayDif;
-	}
-
+	friend int operator-(const Date& x, const Date& y);
 
 	//前置増分演算子
-	Date& operator++() {
-		//日がその月の最大日より小さいとき
-		if(d < days_of_month(y, m)) {
-			d++;
-		//大きいとき
-		} else {
-			//1増やした月が12より大きいとき1年増やして月を1月にリセット
-			if(++m < 12) {
-				y++;
-				m = 1;
-			}
-			//日にちを1日増やす
-			d += 1;
-		}
-		//増やした後のオブジェクトを返却
-		return *this;
-	}
-
+	Date& operator++();
 
 	//後置増分演算子
-	Date operator++(int) {
-		//インクリメントされる前のデータを避難
-		Date DateReturn = *this;
-		//その時のデータメンバを一日増やす
-		++*this;
-		//避難して置いたデータを返却
-		return DateReturn;
-	}
-
+	Date operator++(int);
 
 	//前置減分演算子
-	Date& operator--() {
-		//日のデータが1以上の時はそのままデクリメント
-		if(d > 1) {
-			d--;
-		//1以下の時
-		} else {
-			//1減らした月が1より小さいとき
-			if(--m < 1) {
-				y--;		//1年減らして
-				m = 12;		//月を12月にセット
-			}
-			//その月の最大の日にちにセット
-			d = days_of_month(y, m);
-		}
-		//セット後のオブジェクトの参照を返却
-		return *this;
-	}
+	Date& operator--();
 
 
 	//後置減分演算子
-	Date operator--(int) {
-		Date DateReturn = *this;
-		--*this;
-		return DateReturn;
-	}
+	Date operator--(int);
 
 	//n日後を求める加算演算子
-	friend Date operator+(Date& x, int n) {
-		int y2 = x.y;	//オブジェクトの年で初期化
-		int m2 = x.m;	//オブジェクトの月で初期化
-		int d2 = x.d;	//オブジェクトの日で初期化
-		int dayAfter = d2 + n;		//足した後の日にちの変数
+	friend Date operator+(Date& x, int n);
 
-		//足した後がその月の最大日より小さいとき
-		if(dayAfter <= days_of_month(y2,m2)) {
-			//そのまま用いる
-			d2 = dayAfter;
-		//大きいとき
-		} else {
-			//足した後の日がその月の最大より小さくなるまで繰り返す
-			for(; dayAfter > days_of_month(y2,m2);) {
-				//12月の時は1年増やして月を1月にリセットして日をその月の最大で引く
-				if(m2 == 12) {
-					dayAfter -= days_of_month(y2,m2);
-					m2 =1;
-					y2++;
-				//12以外の月は最大の日で引いて月を1増やす
-				} else {
-					dayAfter -= days_of_month(y2,m2);
-					m2++;
-				}
-			}
-			//最大以下になったのを代入
-			d2 = dayAfter;
-		}
-		//出来た日付で作ったオブジェクトを返却
-		return Date(y2,m2,d2);
-	}
-
-	friend Date operator- (const Date& x, int n) {
-		int y2 = x.y;		//その年で初期化
-		int m2 = x.m;		//その月で初期化
-		int d2 = x.d;		//その日にちで初期化
-		int dayBefore = d2 - n;		//日を引いた後の変数を宣言
-
-		//引いた後の日が0より大きいときはそのまま
-		if(dayBefore > 0) {
-			d2 = dayBefore;
-		//小さいとき
-		} else {
-			//引いた日にちが0より大きくなるまで繰り返す
-			for(; dayBefore < 0;) {
-				//1月の時は12月の最大で足して12月にリセットして年を1年減らす
-				if(m2 == 1) {
-					dayBefore += days_of_month(y2,12);
-					m2 = 12;
-					y2--;
-				//1月以外はその前の月の最大で足して月を1つ減らす
-				} else {
-					dayBefore +=days_of_month(y2,m2 - 1);
-					m2--;
-				}
-			}
-			//0よりい大きくなった時のものを代入
-			d2 = dayBefore;
-		}
-		//出来上がったデータで作ったオブジェクトを返却
-		return Date(y2,m2,d2);
-	}
-
+	//n日前を求める減算演算子
+	friend Date operator- (const Date& x, int n);
 
 	//上で求めた演算子を使ってそれで代入する
 	friend void operator+=( Date& x, int n) {
--- e_13_01/src/e_13_01_sourse.cpp
+++ e_13_01/src/e_13_01_sourse.cpp
@@ -145,7 +145,7 @@ istream& operator>>(istream& s, Date& x)
 		std::cout << "日：";
 		//キーボードから入力
 		s >> d;
-	}while(d < 1 || 31 < d);
+	}while(d < 1 || x.days_of_month2(y,m) < d);
 
 	x.year_set(y);		//セッターのメンバ関数を呼び出して年を設定する
 	x.month_set(m);		//セッターのメンバ関数を呼び出して月を設定する
@@ -153,4 +153,235 @@ istream& operator>>(istream& s, Date& x)
 
 	return s;			//入力ストリームsを返却
 }
+
+//後者のオブジェクトのほうが大きいかをbool型で返却する演算子関数
+bool operator< (const Date& x, const Date& y) {
+	int nReturn;	//返却値
+	//年がｙのほうが大きいとき
+	if(x.y < y.y){
+		nReturn = 1;
+		//年がyのほうが小さいとき
+	} else if(x.y > y.y) {
+		nReturn = 0;
+		//年が同じとき
+	} else {
+		//月がyのほうが大きいとき
+		if(x.m < y.m){
+			nReturn = 1;
+			//月がyのほうが小さいとき
+		} else if(x.m > y.m) {
+			nReturn = 0;
+			//月も同じとき
+		} else {
+			//日がyのほうが大きいとき
+			if(x.d < y.d){
+				nReturn = 1;
+				//日まで同じかyのほうが小さいとき
+			} else {
+				nReturn = 0;
+			}
+		}
+	}
+	//返却
+	return nReturn;
+}
+
+
+bool operator> (const Date& x, const Date& y) {
+	int nReturn;	//返却値
+	//年がxのほうが大きいとき
+	if(x.y > y.y){
+		nReturn = 1;
+		//年がxのほうが小さいとき
+	} else if(x.y < y.y) {
+		nReturn = 0;
+		//年が同じとき
+	} else {
+		//月がxのほうが大きいとき
+		if(x.m > y.m){
+			nReturn = 1;
+			//月がxのほうが小さいとき
+		} else if(x.m < y.m) {
+			nReturn = 0;
+			//月も同じとき
+		} else {
+			//日がxのほうが大きいとき
+			if(x.d > y.d){
+				nReturn = 1;
+				//日まで同じかxのほうが小さいとき
+			} else {
+				nReturn = 0;
+			}
+		}
+	}
+	//返却
+	return nReturn;
+}
+
+//上で定義した2つを使ってどちらかを満たすとき真をbool型で返却
+bool operator<= (const Date& x, const Date& y) {
+	return x < y || x == y;
+}
+
+//上で定義した2つを使ってどちらかを満たすとき真をbool型で返却
+bool operator>= (const Date& x, const Date& y) {
+	return x > y || x == y;
+}
+
+int operator-(const Date& x, const Date& y) {
+	//返却する離れている日数
+	int dayDif = 0;
+	//大きいほう
+	int y1 = x > y ? x.y : y.y;
+	int m1 = x > y ? x.m : y.m;
+	//小さいほう
+	int y2 = x < y ? x.y : y.y;
+	int m2 = x < y ? x.m : y.m;
+	int d2 = x < y ?  y.d - x.d : x.d - y.d;
+
+	//同値の時0を返却
+	if(x == y) {
+		dayDif = 0;
+	//同値ではない時
+	} else  {
+		//小さいほうの日付が大きいほうと年と月が一致するまで繰り返す
+		while(y2 < y1 || m2 < m1) {
+			//その月の最大の日を足して代入
+			dayDif += x.days_of_month2(y2,m2);
+			//月が12月の時1月に戻して1年進める
+			if(m2 == 12) {
+				m2 = 1;
+				y2++;
+			//月が12月以外はそのまま1増やす
+			} else {
+				m2++;
+			}
+		}
+		//日の差を足して日にちを調節する
+		dayDif += d2;
+	}
+	//yのほうが大きいとき負の値にして返す仕様なのでー1掛ける
+	if(x < y) {
+		dayDif *= -1;
+	}
+	//求めた離れている日数を返却する
+	return dayDif;
+
+}
+
+//前置増分演算子
+Date& Date::operator++() {
+	//オブジェクトに次の日を求める関数から返却された値を代入
+	*this = following_day();
+	//次の日のデータを返却する
+	return *this;
+}
+
+//後置増分演算子
+Date Date::operator++(int) {
+	//インクリメントされる前のデータを避難
+	Date DateReturn = *this;
+	//その時のデータメンバを一日増やす
+	++*this;
+	//避難して置いたデータを返却
+	return DateReturn;
+}
+
+//前置減分演算子
+Date& Date::operator--() {
+	//オブジェクトに前の日を求める関数から返却された値を代入
+	*this = preceding_day();
+	//前の日のデータを返却
+	return *this;
+}
+
+//後置減分演算子
+Date Date::operator --(int) {
+	//デクリメントされる前のデータを避難
+	Date DateReturn = *this;
+	//デクリメントする
+	--*this;
+	//避難して置いたデータを返却
+	return DateReturn;
+}
+
+//n日後を求める加算演算子
+Date operator+(Date& x, int n) {
+	//コピーインストラクタによってコピーオブジェクトを作る
+	Date copy = x;
+
+	copy.d += n;
+	//足した後がその月の最大日より小さいとき
+	if(copy.d <= Date::days_of_month(copy.y,copy.m)) {
+		//引いた日にちが0より大きくなるまで繰り返す
+		for(; copy.d < 0;) {
+			//1月の時は12月の最大で足して12月にリセットして年を1年減らす
+			if(copy.m == 1) {
+				copy.d  += Date::days_of_month(copy.y,12);
+				copy.m = 12;
+				copy.y--;
+			//1月以外はその前の月の最大で足して月を1つ減らす
+			} else {
+				copy.d  += Date::days_of_month(copy.y,copy.m - 1);
+				copy.m--;
+			}
+		}
+	//大きいとき
+	} else {
+		//足した後の日がその月の最大より小さくなるまで繰り返す
+		for(; copy.d > Date::days_of_month(copy.y,copy.m);) {
+			//12月の時は1年増やして月を1月にリセットして日をその月の最大で引く
+			if(x.m == 12) {
+				copy.d -= Date::days_of_month(copy.y,copy.m);
+				copy.m =1;
+				copy.y++;
+				//12以外の月は最大の日で引いて月を1増やす
+			} else {
+				copy.d-= Date::days_of_month(copy.y,copy.m);
+				copy.m++;
+			}
+		}
+	}
+	//出来た日付で作ったオブジェクトを返却
+	return copy;
+}
+
+Date operator- (const Date& x, int n) {
+		Date copy = x;
+		copy.d -= n;
+
+		//引いた後の日が0より大きいとき
+		if(copy.d > 0) {
+			//足した後の日がその月の最大より小さくなるまで繰り返す
+			for(; copy.d > Date::days_of_month(copy.y,copy.m);) {
+				//12月の時は1年増やして月を1月にリセットして日をその月の最大で引く
+				if(copy.m == 12) {
+					copy.d -= Date::days_of_month(copy.y,copy.m);
+					copy.m =1;
+					copy.y++;
+					//12以外の月は最大の日で引いて月を1増やす
+				} else {
+					copy.d -= Date::days_of_month(copy.y,copy.m);
+					copy.m++;
+				}
+			}
+		//小さいとき
+		} else {
+			//引いた日にちが0より大きくなるまで繰り返す
+			for(; copy.d < 0;) {
+				//1月の時は12月の最大で足して12月にリセットして年を1年減らす
+				if(copy.m == 1) {
+					copy.d  += Date::days_of_month(copy.y,12);
+					copy.m = 12;
+					copy.y--;
+				//1月以外はその前の月の最大で足して月を1つ減らす
+				} else {
+					copy.d += Date::days_of_month(copy.y,copy.m - 1);
+					copy.m--;
+				}
+			}
+		}
+		//出来上がったデータで作ったオブジェクトを返却
+		return Date(copy.y,copy.m,copy.d);
+	}
 
