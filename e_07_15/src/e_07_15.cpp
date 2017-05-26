42eaf2e855b6fcaafd2a15439795dd9f606982dc
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-26 18:41:26 +0900

    修正コミット　演習07-15　（進捗率100%）修正した後プッシュし忘れていました　作成者　成田修之

--- e_07_15/src/e_07_15.cpp
+++ e_07_15/src/e_07_15.cpp
@@ -9,16 +9,28 @@
 using namespace std;
 int main()
 {
+	//動的記憶域期間について初期化
 	double* dblA =new double(0.0);
+	//通常初期化
 	double dblB = 0.1;
+	//new演算子と似た形の初期化
 	double dblC (0.2);
+	//キャスト記法の初期化
 	double dblD = double(0.3);
 
+	//小数点以下を1桁で表示する
 	cout << fixed << setprecision(1);
+	//動的記憶域期間について確認
 	cout << "dblA：" << *dblA << "\n";
-	cout << "dblB：" << dblB << "\n";
-	cout << "dblC：" << dblC << "\n";
-	cout << "dblD：" << dblD << "\n";
+	//通常初期化について確認
+	cout << "dblB：" << dblB  << "\n";
+	//new演算子と似た形の初期化について確認
+	cout << "dblC：" << dblC  << "\n";
+	//キャスト記法の初期化について確認
+	cout << "dblD：" << dblD  << "\n";
+
+	//
+	delete dblA;
 
 	//main関数の返却値
 	return 0;
