bdbcfe2b68b15b0bd97b76dc364270036865cefe
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-05-26 18:41:37 +0900

    修正コミット　演習07-16（進捗率100%）修正した後プッシュし忘れていました　作成者　成田修之

--- e_07_16/src/e_07_16.cpp
+++ e_07_16/src/e_07_16.cpp
@@ -17,15 +17,23 @@ int main()
 	//キーボードから入力
 	cin >> nInputNumber;
 	//以下を試してみる
+
 	try{
 		//入力された要素数の動的な配列を生成
-		double* nArray = new double[nInputNumber];
+		double* nArray = new double[nInputNumber]{0};
+		for(int i = 0; i < nInputNumber; i++){
+			cout << nArray[i] <<"\n";
+		}
+		delete [] nArray;
 
 	//上が出来なかったとき
-	}catch(bad_alloc) {
+	} catch(bad_alloc&) {
 		//生成に失敗したこととプログラムを終了することを表示
 		cout << "生成に失敗したのでプログラムを終了します";
 		//プログラムを終了するための返却値
 		return 1;
 	}
+	return 0;
+
+
 }
